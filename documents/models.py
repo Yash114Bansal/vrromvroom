f9d7ff4fa61458c07e1ba33e27cdc25b36e4764a
yash114bansal <yash114bansal@gmail.com>
2024-01-29 03:20:55 +0530

    add vehicle details

--- documents/models.py
+++ documents/models.py
@@ -26,5 +26,16 @@ class DrivingLicenseModel(DocumentVerification):
         verbose_name = 'Driving License'
 
 class ImageWithVehicleModel(DocumentVerification):
+    
+    VEHICLE_TYPES = (
+        ('2W', '2 Wheeler'),
+        ('4W', '4 Wheeler'),
+    )
+
+    vehicle_type = models.CharField(max_length=2, choices=VEHICLE_TYPES, blank=True, null=True)
+    plate_number = models.CharField(max_length=20, blank=True, null=True)
+    vehicle_model = models.CharField(max_length=255, blank=True, null=True)
+
+
     class Meta:
         verbose_name = 'Image With Vehicle'
