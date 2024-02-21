bbf188586960ff79377158b5d1b7b3406ddf9d79
yash114bansal <yash114bansal@gmail.com>
2024-02-21 14:30:27 +0530

    fix asgi application

--- vroomvroom/asgi.py
+++ vroomvroom/asgi.py
@@ -1,5 +1,3 @@
-# asgi.py
-
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -8,7 +6,6 @@ from channels.auth import AuthMiddlewareStack
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vroomvroom.settings')
 django_asgi_app = get_asgi_application()
 
-# Move this import here
 import chat.routing
 import django
 django.setup()
